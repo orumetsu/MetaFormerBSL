fix averaging bug for many mid low shot acc

main.py
@@ -311,7 +311,7 @@ def train_one_epoch_local_data(config, model, criterion, data_loader, optimizer,
         time_per_batch = time.time() - end
 
         if config.TRAIN.ACCUMULATION_STEPS > 1: # scaling loss and batch time if using step accumulation
-            loss *= config.TRAIN.ACCUMULATION_STEPS # DO I SCALE THIS OR NOT???
+            loss *= config.TRAIN.ACCUMULATION_STEPS
             time_per_batch *= config.TRAIN.ACCUMULATION_STEPS
 
         loss_meter.update(loss.item(), targets.size(0))
@@ -406,9 +406,12 @@ def validate(config, data_loader, model, training_labels, mask_meta=False):
         acc1_meter.update(acc1.item(), target.size(0))
         acc5_meter.update(acc5.item(), target.size(0))
 
-        many_acc_meter.update(many_shot_acc, target.size(0))
-        median_acc_meter.update(median_shot_acc, target.size(0))
-        low_acc_meter.update(low_shot_acc, target.size(0))
+        if many_shot_acc >= 0.:
+            many_acc_meter.update(many_shot_acc, target.size(0))
+        if median_shot_acc >= 0.:
+            median_acc_meter.update(median_shot_acc, target.size(0))
+        if low_shot_acc >= 0.:
+            low_acc_meter.update(low_shot_acc, target.size(0))
         
         if (idx + 1) % config.PRINT_FREQ == 0:
             memory_used = torch.cuda.max_memory_allocated() / (1024.0 * 1024.0)

utils.py
@@ -166,8 +166,9 @@ def load_ext(name, funcs):
         assert hasattr(ext, fun), f'{fun} missing in module {name}'
     return ext
 
+
 # Many-Median-Low Shot Accuracy (from Balanced Softmax Loss)
-def shot_acc (preds, labels, train_data, many_shot_thr=100, low_shot_thr=20, acc_per_cls=False):
+def shot_acc(preds, labels, train_data, many_shot_thr=100, low_shot_thr=20, acc_per_cls=False):
     
     if isinstance(train_data, np.ndarray):
         training_labels = np.array(train_data).astype(int)
@@ -201,11 +202,11 @@ def shot_acc (preds, labels, train_data, many_shot_thr=100, low_shot_thr=20, acc
             median_shot.append((class_correct[i] / test_class_count[i]))    
  
     if len(many_shot) == 0:
-        many_shot.append(0)
+        many_shot.append(-1)
     if len(median_shot) == 0:
-        median_shot.append(0)
+        median_shot.append(-1)
     if len(low_shot) == 0:
-        low_shot.append(0)
+        low_shot.append(-1)
 
     if acc_per_cls:
         class_accs = [c / cnt for c, cnt in zip(class_correct, test_class_count)] 