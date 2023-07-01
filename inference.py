67d4d5f3446d8214566e918122cf946642e894c7
M. Riv'an Arif <77826824+orumetsu@users.noreply.github.com>
2023-07-02 02:22:56 +0700

    inference script for MetaFormerBSL

--- inference.py
+++ inference.py
@@ -1,16 +1,17 @@
-from transformers import AutoTokenizer, AutoModel
 import torch
 from PIL import Image
 from config import get_inference_config
 from models import build_model
-from torch import amp
-from torch.autograd import Variable
 from torchvision.transforms import transforms
 import numpy as np
 import argparse
+from math import radians, cos, sin, pi
+import re
+import json
 
 IMAGENET_DEFAULT_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_DEFAULT_STD = (0.229, 0.224, 0.225)
+
 
 
 class Namespace:
@@ -25,97 +26,117 @@ def model_config(config_path):
 
 
 def read_class_names(file_path):
-    file = open(file_path, 'r')
-    lines = file.readlines()
-    class_list = []
+    with open(file_path, 'r') as file:
+        categories = json.load(file)
+    
+    classes = {}
+    for category in categories:
+        classes[category['id']] = category['name']
 
-    for l in lines:
-        line = l.strip().split()
-        # class_list.append(line[0])
-        class_list.append(line[1][4:])
-
-    classes = tuple(class_list)
     return classes
 
 
-class GenerateEmbedding:
-    def __init__(self, text_file):
-        self.text_file = text_file
+def get_spatial_info(latitude, longitude):
+    if latitude and longitude:
+        latitude = radians(latitude)
+        longitude = radians(longitude)
+        x = cos(latitude)*cos(longitude)
+        y = cos(latitude)*sin(longitude)
+        z = sin(latitude)
+        return [x,y,z]
+    else:
+        return [0,0,0]
 
-        self.tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-        self.model = AutoModel.from_pretrained("bert-base-uncased")
 
-    def generate(self):
-        text_list = []
-        with open(self.text_file, 'r') as f_text:
-            for line in f_text:
-                line = line.encode(encoding='UTF-8', errors='strict')
-                line = line.replace(b'\xef\xbf\xbd\xef\xbf\xbd', b' ')
-                line = line.decode('UTF-8', 'strict')
-                text_list.append(line)
-            # data = f_text.read()
-        select_index = np.random.randint(len(text_list))
-        inputs = self.tokenizer(text_list[select_index], return_tensors="pt", padding="max_length",
-                                truncation=True, max_length=32)
-        outputs = self.model(**inputs)
-        embedding_mean = outputs[1].mean(dim=0).reshape(1, -1).detach().numpy()
-        embedding_full = outputs[1].detach().numpy()
-        embedding_words = outputs[0] # outputs[0].detach().numpy()
-        return None, None, embedding_words
+def get_temporal_info(date, miss_hour=False):
+    try:
+        if date:
+            if miss_hour:
+                pattern = re.compile(r'(\d*)-(\d*)-(\d*)', re.I)
+            else:
+                pattern = re.compile(r'(\d*)-(\d*)-(\d*) (\d*):(\d*):(\d*)', re.I)
+            m = pattern.match(date.strip())
+
+            if m:
+                year = int(m.group(1))
+                month = int(m.group(2))
+                day = int(m.group(3))
+                x_month = sin(2*pi*month/12)
+                y_month = cos(2*pi*month/12) 
+                if miss_hour:
+                    x_hour = 0
+                    y_hour = 0
+                else:
+                    hour = int(m.group(4))
+                    x_hour = sin(2*pi*hour/24)
+                    y_hour = cos(2*pi*hour/24)        
+                return [x_month,y_month,x_hour,y_hour]
+            else:
+                return [0,0,0,0]
+        else:
+            return [0,0,0,0]
+    except:
+        return [0,0,0,0]
 
 
 class Inference:
     def __init__(self, config_path, model_path):
         self.config_path = config_path
         self.model_path = model_path
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        # self.classes = ("cat", "dog")
-        self.classes = read_class_names(r"D:\dataset\CUB_200_2011\CUB_200_2011\classes_custom.txt")
 
+        # Model Building
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.classes = read_class_names(r"./MetaFormerBSL/datasets/inaturalist2018/categories.json")
+        print(self.classes)
         self.config = model_config(self.config_path)
         self.model = build_model(self.config)
         self.checkpoint = torch.load(self.model_path, map_location='cpu')
-        self.model.load_state_dict(self.checkpoint['model'], strict=False)
+        msg = self.model.load_state_dict(self.checkpoint['model'], strict=False)
+        print(msg)
         self.model.eval()
-        self.model.cuda()
-
+        self.model.to(self.device)
+        
+        # Image Preprocessing
+        self.img_size = 224
+        self.crop_resize = int((256 / 224) * self.img_size)
         self.transform_img = transforms.Compose([
-            transforms.Resize((224, 224), interpolation=Image.BILINEAR),
-            transforms.ToTensor(), # transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            transforms.Resize(self.crop_resize, interpolation=Image.BICUBIC),
+            transforms.CenterCrop(self.img_size),
+            transforms.ToTensor(),
             transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD)
         ])
 
-    def infer(self, img_path, meta_data_path):
-        _, _, meta = GenerateEmbedding(meta_data_path).generate()
-        meta = meta.cuda()
-        img = Image.open(img_path).convert('RGB')
-        img = self.transform_img(img)
-        img.unsqueeze_(0)
-        img = img.cuda()
-        img = Variable(img).to(self.device)
-        out = self.model(img, meta)
+    def predict(self, img_path, location): ###
+        temporal_info = [0, 0, 0, 0] # get_temporal_info(date, miss_hour=True) #
+        spatial_info = get_spatial_info(location[0], location[1])
+        meta = temporal_info + spatial_info
+        meta = meta.to(self.device)
 
-        _, pred = torch.max(out.data, 1)
-        predict = self.classes[pred.data.item()]
-        # print(Fore.MAGENTA + f"The Prediction is: {predict}")
-        return predict
+        image = Image.open(img_path).convert('RGB')
+        image = self.transform_img(image)
+        image.unsqueeze_(0)
+        image = image.to(self.device)
+
+        output = self.model(image, meta)
+        _, pred = torch.max(output.data, 1)
+        prediction = self.classes[pred.data.item()]
+        return prediction
 
 
 def parse_option():
-    parser = argparse.ArgumentParser('MetaFG Inference script', add_help=False)
-    parser.add_argument('--cfg', type=str, default='D:/pycharmprojects/MetaFormer/configs/MetaFG_meta_bert_1_224.yaml', metavar="FILE", help='path to config file', )
-    # easy config modification
-    parser.add_argument('--model-path', default='D:\pycharmprojects\MetaFormer\output\MetaFG_meta_1\cub_200\ckpt_epoch_92.pth', type=str, help="path to model data")
-    parser.add_argument('--img-path', default=r"D:\dataset\CUB_200_2011\CUB_200_2011\images\012.Yellow_headed_Blackbird\Yellow_Headed_Blackbird_0003_8337.jpg", type=str, help='path to image')
-    parser.add_argument('--meta-path', default=r"D:\dataset\CUB_200_2011\text_c10\012.Yellow_headed_Blackbird\Yellow_Headed_Blackbird_0003_8337.txt", type=str, help='path to meta data')
+    parser = argparse.ArgumentParser('MetaFormerBSL Inference Script', add_help=False)
+    parser.add_argument('--cfg', type=str, metavar="FILE", help='Path to Config File', )
+    parser.add_argument('--model-path', type=str, help="Path to Model Weights")
+    parser.add_argument('--img-path', type=str, help='Path to Image')
+    parser.add_argument('--location', type=tuple, help='(latitude, longitude) in degrees')
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
     args = parse_option()
-    result = Inference(config_path=args.cfg,
-                       model_path=args.model_path).infer(img_path=args.img_path, meta_data_path=args.meta_path)
-    print("Predicted: ", result)
+    result = Inference(config_path=args.cfg, model_path=args.model_path).predict(img_path=args.img_path, location=args.location)
+    print("Predicted:", result)
 
 # Usage: python inference.py --cfg 'path/to/cfg' --model_path 'path/to/model' --img-path 'path/to/img' --meta-path 'path/to/meta'
+# 
